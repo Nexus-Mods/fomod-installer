Nexus-Mods/fomod-installer

commit 6098ae6f3b4162d807d108d92801c9fa2bd87a84

provide reason when option isn't available

InstallScripting/XmlScript/CompositeCondition.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Text;
@@ -129,40 +130,33 @@ namespace FomodInstaller.Scripting.XmlScript
 		///		File '&lt;file>' is not &lt;state>.
 		/// </remarks>
 		/// <param name="coreDelegates">The Core delegates component.</param>
+    /// <param name="invert">Invert the logic in the message, explaining why it passed instead of why not</param>
 		/// <returns>A message describing whether or not the condition is fulfilled.</returns>
 		/// <seealso cref="ICondition.GetMessage(CoreDelegates)"/>
-		public string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates)
+		public string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates, bool invert)
 		{
-			StringBuilder stbMessage = new StringBuilder();
-			if (m_dopOperator == ConditionOperator.Or)
-				stbMessage.Append("(");
-
 			bool booAllFulfilled = (m_dopOperator == ConditionOperator.And) ? true : false;
 			bool booThisFulfilled = true;
 			ICondition conCondition = null;
+
+      List<string> lines = new List<string>();
+
 			for (Int32 i = 0; i < m_lstConditions.Count; i++)
 			{
 				conCondition = m_lstConditions[i];
 				booThisFulfilled = conCondition.GetIsFulfilled(csmState, coreDelegates);
 				if (!booThisFulfilled)
-					stbMessage.Append(conCondition.GetMessage(csmState, coreDelegates));
-				switch (m_dopOperator)
-				{
-					case ConditionOperator.And:
-						if (i < m_lstConditions.Count - 1)
-							stbMessage.AppendLine();
-						booAllFulfilled &= booThisFulfilled;
-						break;
-					case ConditionOperator.Or:
-						if (i < m_lstConditions.Count - 1)
-							stbMessage.AppendLine(" OR");
-						booAllFulfilled |= booThisFulfilled;
-						break;
-				}
+          lines.Add(conCondition.GetMessage(csmState, coreDelegates, invert));
+
+        booAllFulfilled = m_dopOperator == ConditionOperator.And
+          ? booAllFulfilled & booThisFulfilled
+          : booAllFulfilled | booThisFulfilled;
 			}
-			if (m_dopOperator == ConditionOperator.Or)
-				stbMessage.Append(")");
-			return booAllFulfilled ? "Passed" : stbMessage.ToString();
+
+      string sep = (m_dopOperator == ConditionOperator.Or) ? " OR\n" : "\n";
+      string message = lines.Aggregate((lhs, rhs) => lhs + sep + rhs);
+
+			return booAllFulfilled && !invert ? "Passed" : message;
 		}
 
 		#endregion

FomodInstaller.Interface/ModInstaller/CoreDelegates.cs
@@ -194,8 +194,9 @@ namespace FomodInstaller.Interface
             public string description;
             public string image;
             public string type;
+            public string conditionMsg;
 
-            public Option(int id, string name, string description, string image, bool selected, string type) : this()
+            public Option(int id, string name, string description, string image, bool selected, string type, string conditionMsg) : this()
             {
                 this.id = id;
                 this.name = name;
@@ -203,6 +204,7 @@ namespace FomodInstaller.Interface
                 this.image = image;
                 this.selected = selected;
                 this.type = type;
+                this.conditionMsg = conditionMsg;
             }
         }
 

FomodInstaller.Interface/Scripting/ScriptFunctionProxy.cs
@@ -382,7 +382,7 @@ namespace FomodInstaller.Interface
             try
             {
                 Core.ui.ReportError(p_strTitle, p_strMessage, p_strDetails);
-            } catch (Exception e)
+            } catch (Exception)
             {
                 // very rare situation, presumable the installer has already quit and the delegates
                 // garbage collected on the JS side - not much to do then

InstallScripting/ModScript/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Nexus.Client.ModManagement.Scripting.ModScript.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {

InstallScripting/XmlScript/ConditionalOptionTypeResolver.cs
@@ -1,166 +1,190 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using FomodInstaller.Interface;
 
 namespace FomodInstaller.Scripting.XmlScript
 {
-	/// <summary>
-	/// An option type that is dependent upon the state of external conditions.
-	/// </summary>
-	public class ConditionalOptionTypeResolver : IOptionTypeResolver
-	{
-		/// <summary>
-		/// A pattern that is matched against external conditions to determine whether
-		/// or not its option type is elected.
-		/// </summary>
-		public class ConditionalTypePattern
-		{
-			private OptionType m_ptpType = OptionType.NotUsable;
-			private ICondition m_cndCondition = null;
+  /// <summary>
+  /// An option type that is dependent upon the state of external conditions.
+  /// </summary>
+  public class ConditionalOptionTypeResolver : IOptionTypeResolver
+  {
+    /// <summary>
+    /// A pattern that is matched against external conditions to determine whether
+    /// or not its option type is elected.
+    /// </summary>
+    public class ConditionalTypePattern
+    {
+      private OptionType m_ptpType = OptionType.NotUsable;
+      private ICondition m_cndCondition = null;
 
-			#region Properties
+      #region Properties
 
-			/// <summary>
-			/// Gets or sets the option type this pattern returns if it is fulfilled.
-			/// </summary>
-			/// <value>The option type this pattern returns if it is fulfilled.</value>
-			public OptionType Type
-			{
-				get
-				{
-					return m_ptpType;
-				}
-				set
-				{
-					m_ptpType = value;
-				}
-			}
+      /// <summary>
+      /// Gets or sets the option type this pattern returns if it is fulfilled.
+      /// </summary>
+      /// <value>The option type this pattern returns if it is fulfilled.</value>
+      public OptionType Type
+      {
+        get
+        {
+          return m_ptpType;
+        }
+        set
+        {
+          m_ptpType = value;
+        }
+      }
 
-			/// <summary>
-			/// Gets or sets the condition that must by fulfilled for this pattern's option type
-			/// to be elected.
-			/// </summary>
-			/// <value>The condition that must by fulfilled for this pattern's option type
-			/// to be elected.</value>
-			public ICondition Condition
-			{
-				get
-				{
-					return m_cndCondition;
-				}
-				set
-				{
-					m_cndCondition = value;
-				}
-			}
+      /// <summary>
+      /// Gets or sets the condition that must by fulfilled for this pattern's option type
+      /// to be elected.
+      /// </summary>
+      /// <value>The condition that must by fulfilled for this pattern's option type
+      /// to be elected.</value>
+      public ICondition Condition
+      {
+        get
+        {
+          return m_cndCondition;
+        }
+        set
+        {
+          m_cndCondition = value;
+        }
+      }
 
-			#endregion
+      #endregion
 
-			#region Constructors
+      #region Constructors
 
-			/// <summary>
-			/// A simple constructor that initializes the object with the given values.
-			/// </summary>
-			/// <param name="p_ptpType">The option type this pattern returns if it is fulfilled.</param>
-			/// <param name="p_cndCondition">The condition that must by fulfilled for this pattern's option type
-			/// to be elected.</param>
-			public ConditionalTypePattern(OptionType p_ptpType, ICondition p_cndCondition)
-			{
-				m_ptpType = p_ptpType;
-				m_cndCondition = p_cndCondition;
-			}
+      /// <summary>
+      /// A simple constructor that initializes the object with the given values.
+      /// </summary>
+      /// <param name="p_ptpType">The option type this pattern returns if it is fulfilled.</param>
+      /// <param name="p_cndCondition">The condition that must by fulfilled for this pattern's option type
+      /// to be elected.</param>
+      public ConditionalTypePattern(OptionType p_ptpType, ICondition p_cndCondition)
+      {
+        m_ptpType = p_ptpType;
+        m_cndCondition = p_cndCondition;
+      }
 
-			#endregion
-		}
+      #endregion
+    }
 
-		private OptionType m_ptpDefaultType = OptionType.NotUsable;
-		private List<ConditionalTypePattern> m_lstPatterns = new List<ConditionalTypePattern>();
+    private OptionType m_ptpDefaultType = OptionType.NotUsable;
+    private List<ConditionalTypePattern> m_lstPatterns = new List<ConditionalTypePattern>();
 
-		#region Properties
+    #region Properties
 
-		/// <summary>
-		/// Gets or sets the default type.
-		/// </summary>
-		/// <remarks>
-		/// The default type is returned if none of the <see cref="ConditionalTypePatterns"/>
-		/// are matched.
-		/// </remarks>
-		/// <value>The default type.</value>
-		public OptionType DefaultType
-		{
-			get
-			{
-				return m_ptpDefaultType;
-			}
-			set
-			{
-				m_ptpDefaultType = value;
-			}
-		}
+    /// <summary>
+    /// Gets or sets the default type.
+    /// </summary>
+    /// <remarks>
+    /// The default type is returned if none of the <see cref="ConditionalTypePatterns"/>
+    /// are matched.
+    /// </remarks>
+    /// <value>The default type.</value>
+    public OptionType DefaultType
+    {
+      get
+      {
+        return m_ptpDefaultType;
+      }
+      set
+      {
+        m_ptpDefaultType = value;
+      }
+    }
 
-		/// <summary>
-		/// Gets the list of conditional patterns that are used to determine the type.
-		/// </summary>
-		/// <value>The list of conditional patterns that are used to determine the type.</value>
-		public List<ConditionalTypePattern> ConditionalTypePatterns
-		{
-			get
-			{
-				return m_lstPatterns;
-			}
-		}
+    /// <summary>
+    /// Gets the list of conditional patterns that are used to determine the type.
+    /// </summary>
+    /// <value>The list of conditional patterns that are used to determine the type.</value>
+    public List<ConditionalTypePattern> ConditionalTypePatterns
+    {
+      get
+      {
+        return m_lstPatterns;
+      }
+    }
 
 
-		#endregion
+    #endregion
 
-		#region Constructors
+    #region Constructors
 
-		/// <summary>
-		/// A simple constructor that initializes the object with the given values.
-		/// </summary>
-		/// <param name="p_ptpDefaultType">The default <see cref="OptionType"/> to return
-		/// if no patterns are fulfilled.</param>
-		public ConditionalOptionTypeResolver(OptionType p_ptpDefaultType)
-		{
-			m_ptpDefaultType = p_ptpDefaultType;
-		}
+    /// <summary>
+    /// A simple constructor that initializes the object with the given values.
+    /// </summary>
+    /// <param name="p_ptpDefaultType">The default <see cref="OptionType"/> to return
+    /// if no patterns are fulfilled.</param>
+    public ConditionalOptionTypeResolver(OptionType p_ptpDefaultType)
+    {
+      m_ptpDefaultType = p_ptpDefaultType;
+    }
 
-		#endregion
+    #endregion
 
-		/// <summary>
-		/// Adds a pattern that returns the given option type if the given condition is fulfilled.
-		/// </summary>
-		/// <param name="p_ptpType">The type the pattern will return if the condition is fulfilled.</param>
-		/// <param name="p_cndCondition">The condition that must be fulfilled in order for the pattern
-		/// to return the option type.</param>
-		public void AddPattern(OptionType p_ptpType, ICondition p_cndCondition)
-		{
-			m_lstPatterns.Add(new ConditionalTypePattern(p_ptpType, p_cndCondition));
-		}
+    /// <summary>
+    /// Adds a pattern that returns the given option type if the given condition is fulfilled.
+    /// </summary>
+    /// <param name="p_ptpType">The type the pattern will return if the condition is fulfilled.</param>
+    /// <param name="p_cndCondition">The condition that must be fulfilled in order for the pattern
+    /// to return the option type.</param>
+    public void AddPattern(OptionType p_ptpType, ICondition p_cndCondition)
+    {
+      m_lstPatterns.Add(new ConditionalTypePattern(p_ptpType, p_cndCondition));
+    }
 
-        #region IOptionTypeResolver Members
+    #region IOptionTypeResolver Members
 
-        /// <summary>
-        /// Gets the plugin type.
-        /// </summary>
-        /// <remarks>
-        /// The returned type is dependent upon external state. A list of patterns are matched
-        /// against external state (e.g., installed files); the first pattern that is fulfilled
-        /// determines the returned type.
-        /// 
-        /// If no pattern is fulfilled, a default type if returned.
-        /// </remarks>
-        /// <param name="coreDelegates">The Core delegates component.</param>
-        /// <returns>The option type.</returns>
-        /// <seealso cref="IOptionTypeResolver.ResolveOptionType(CoreDelegates)"/>
-        public OptionType ResolveOptionType(ConditionStateManager csmState, CoreDelegates coreDelegates)
-		{
-			foreach (ConditionalTypePattern ctpPattern in m_lstPatterns)
-				if (ctpPattern.Condition.GetIsFulfilled(csmState, coreDelegates))
-					return ctpPattern.Type;
-			return m_ptpDefaultType;
-		}
+    /// <summary>
+    /// Gets the plugin type.
+    /// </summary>
+    /// <remarks>
+    /// The returned type is dependent upon external state. A list of patterns are matched
+    /// against external state (e.g., installed files); the first pattern that is fulfilled
+    /// determines the returned type.
+    /// 
+    /// If no pattern is fulfilled, a default type if returned.
+    /// </remarks>
+    /// <param name="coreDelegates">The Core delegates component.</param>
+    /// <returns>The option type.</returns>
+    /// <seealso cref="IOptionTypeResolver.ResolveOptionType(CoreDelegates)"/>
+    public OptionType ResolveOptionType(ConditionStateManager csmState, CoreDelegates coreDelegates)
+    {
+      foreach (ConditionalTypePattern ctpPattern in m_lstPatterns)
+        if (ctpPattern.Condition.GetIsFulfilled(csmState, coreDelegates))
+        {
+          return ctpPattern.Type;
+        }
+      return m_ptpDefaultType;
+    }
 
-		#endregion
-	}
+    public string ResolveConditionMessage(ConditionStateManager csmState, CoreDelegates coreDelegates)
+    {
+      List<string> unmatched = new List<string>();
+
+      foreach (ConditionalTypePattern ctpPattern in m_lstPatterns) {
+        if (ctpPattern.Condition.GetIsFulfilled(csmState, coreDelegates))
+        {
+          if (ctpPattern.Type == OptionType.NotUsable)
+          {
+            return ctpPattern.Condition.GetMessage(csmState, coreDelegates, true);
+          } else
+          {
+            return null;
+          }
+        } else if (ctpPattern.Type != OptionType.NotUsable) {
+          unmatched.Add("Not: " + ctpPattern.Condition.GetMessage(csmState, coreDelegates, true));
+        }
+      }
+      return unmatched.Aggregate((i, j) => i + "\n" + j);
+    }
+
+    #endregion
+  }
 }

InstallScripting/XmlScript/FlagCondition.cs
@@ -95,11 +95,11 @@ namespace FomodInstaller.Scripting.XmlScript
         /// <param name="coreDelegates">The Core delegates component.</param>
         /// <returns>A message describing whether or not the condition is fulfilled.</returns>
         /// <seealso cref="ICondition.GetMessage(CoreDelegates)"/>
-		public string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates)
+		public string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates, bool invert)
 		{
-			if (GetIsFulfilled(csmState, coreDelegates))
+			if (GetIsFulfilled(csmState, coreDelegates) && !invert)
 				return "Passed";
-			return string.Format("Flag '{0}' is not {1}.", FlagName, Value);
+			return string.Format("Flag '{0}' is {2}{1}.", FlagName, Value, invert ? "" : "not ");
 		}
 
 		#endregion

InstallScripting/XmlScript/GameVersionCondition.cs
@@ -60,7 +60,7 @@ namespace FomodInstaller.Scripting.XmlScript
         /// <param name="coreDelegates">The Core delegates component.</param>
         /// <returns>A message describing whether or not the condition is fulfilled.</returns>
         /// <seealso cref="ICondition.GetMessage(CoreDelegates)"/>
-		public override string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates)
+		public override string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates, bool invert)
 		{
             Version GameVersion = new Version("0.0.0.0");
 
@@ -73,7 +73,7 @@ namespace FomodInstaller.Scripting.XmlScript
                     GameVersion = new Version("0.0.0.0");
             }).Wait();
 
-            if (GameVersion < MinimumVersion)
+            if ((GameVersion < MinimumVersion) && !invert)
 				return string.Format("This mod requires v{0} or higher of the game. You have {1}. Please update your game.", MinimumVersion, GameVersion);
 			return "Passed";
 		}

InstallScripting/XmlScript/ICondition.cs
@@ -31,6 +31,6 @@ namespace FomodInstaller.Scripting.XmlScript
         /// <param name="coreDelegates">The Core delegates component.</param>
         /// <returns>A message describing whether or not the condition is fulfilled.</returns>
         /// <seealso cref="ICondition.GetMessage(CoreDelegates)"/>
-		string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates);
+		string GetMessage(ConditionStateManager csmState, CoreDelegates coreDelegates, bool invert);
 	}
 }

commit 942a05d4830e5362adaee60db7cdf117c97b02a5

6098ae6f3b4162d807d108d92801c9fa2bd87a84 introduced error installing some mods

InstallScripting/XmlScript/CompositeCondition.cs
@@ -154,7 +154,7 @@ namespace FomodInstaller.Scripting.XmlScript
 			}
 
       string sep = (m_dopOperator == ConditionOperator.Or) ? " OR\n" : "\n";
-      string message = lines.Aggregate((lhs, rhs) => lhs + sep + rhs);
+      string message = string.Join(sep, lines);
 
 			return booAllFulfilled && !invert ? "Passed" : message;
 		}