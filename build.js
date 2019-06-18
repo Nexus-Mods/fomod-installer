d0c0906df295c027adc2576ac14fb72ef5312f3e
TanninOne <seppleviathan@gmx.de>
2019-06-18 10:28:27 +0200

    build fix attempt nr 768

--- build.js
+++ build.js
@@ -1,12 +1,27 @@
 var msbuildLib = require('msbuild');
 var path = require('path');
 
-var msbuild = new msbuildLib();
-msbuild.sourcePath = path.join(__dirname, 'FomodInstaller.sln');
+function build() {
+  var msbuild = new msbuildLib();
+  msbuild.sourcePath = path.join(__dirname, 'FomodInstaller.sln');
 
-msbuild.configuration = process.argv[2] || 'Release';
-msbuild.configuration += ';TargetFrameworkVersion=v4.5.2';
-msbuild.overrideParams.push('/m'); // parallel build
-msbuild.overrideParams.push('/clp:ErrorsOnly');
+  msbuild.configuration = process.argv[2] || 'Release';
+  msbuild.configuration += ';TargetFrameworkVersion=v4.5.2';
+  msbuild.overrideParams.push('/m'); // parallel build
+  msbuild.overrideParams.push('/clp:ErrorsOnly');
 
-msbuild.build();
+  msbuild.build();
+}
+
+
+function restore(cb) {
+  var msbuild = new msbuildLib(cb);
+
+  msbuild.configuration = process.argv[2] || 'Release';
+  msbuild.configuration += ';TargetFrameworkVersion=v4.5';
+  msbuild.overrideParams.push('/t:restore');
+  msbuild.build();
+}
+
+restore(() => build());
+
