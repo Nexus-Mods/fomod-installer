83d634dd0a51708187e1ece493b1831ac0b24e08
TanninOne <seppleviathan@gmx.de>
2020-12-02 12:23:40 +0100

    more robust spawn in case the process fails to start

--- index.js
+++ index.js
@@ -10,20 +10,32 @@ async function createIPC(usePipe, id) {
     if (usePipe) {
       args.push('--pipe');
     }
-    const proc = cp.spawn(path.join(__dirname, 'dist', exeName), args);
-    proc.on('error', err => {
-      reject(err);
-    });
-    proc.on('exit', (code, signal) => {
-      if (code === 0x80131700) {
-        reject(new Error('No compatible .Net Framework, you need .Net framework 4.6 or newer'));
-      } else if (code !== null) {
-        reject(new Error(`Failed to run fomod installer. Errorcode ${code.toString(16)}`));
+    const proc = cp.spawn(path.join(__dirname, 'dist', exeName), args)
+      .on('error', err => {
+        reject?.(err);
+        resolve = reject = undefined;
+      })
+      .on('exit', (code, signal) => {
+        if (code === 0x80131700) {
+          reject?.(new Error('No compatible .Net Framework, you need .Net framework 4.6 or newer'));
+        } else if (code !== null) {
+          reject?.(new Error(`Failed to run fomod installer. Errorcode ${code.toString(16)}`));
+        } else {
+          reject?.(new Error(`The fomod installer was terminated. Signal: ${signal}`));
+        }
+        resolve = reject = undefined;
+      });
+
+    // resolve slightly delayed to allow the error event to be triggered if the process fails to
+    // start. Unfortunately cp.spawn seems to flip a coin on whether it reports events at all or not.
+    setTimeout(() => {
+      if ((proc.exitCode !== null) && (proc.exitCode !== 0)) {
+        reject?.(new Error('Failed to spawn fomod installer'));
       } else {
-        reject(new Error(`The fomod installer was terminated. Signal: ${signal}`));
+        resolve?.(proc);
       }
-    });
-    resolve(proc);
+      resolve = reject = undefined;
+    }, 100);
   });
 }
 
