512c2f6455ec222a26880840c566b047dc9be97a
NutSoLibre <m.raer6@gmail.com>
2024-11-15 11:55:51 +0000

    removing IPC component and adding JS packages

--- build.js
+++ build.js
@@ -1,9 +1,5 @@
-const cp = require('child_process');
-const fs = require('fs');
-const path = require('path');
-const fsExtra = require('fs-extra');
-
-const debugBuild = false;
+const semver = require('semver');
+const dotnet = require('node-api-dotnet');
 
 class MissingDotNetSDKException extends Error {
   constructor() {
@@ -18,74 +14,12 @@ const ERROR_CODE_HANDLER = {
   },
 }
 
-function spawnAsync(exe, args, options, out) {
-  if (options === undefined) {
-    options = {};
-  }
-  if (out === undefined) {
-    out = console;
-  }
-  return new Promise((resolve, reject) => {
-    let desc = `${options.cwd || '.'}/${exe} ${args.join(' ')}`;
-    out.log('started: ' + desc);
-    const outBufs = [];
-    try {
-      let proc = cp.spawn(exe, args, options);
-      proc.stdout.on('data', (data) => outBufs.push(data));
-      proc.stderr.on('data', (data) => out.error(data.toString()));
-      proc.on('error', (err) => {
-        out.log(Buffer.concat(outBufs).toString());
-        reject(err);
-      });
-      proc.on('close', (code) => {
-        out.log('done: ' + desc + ': ' + code);
-        if (code === 0) {
-          resolve();
-        } else {
-          out.log(Buffer.concat(outBufs).toString());
-          reject(new Error(`${desc} failed with code ${code}`));
-        }
-      });
-    } catch (err) {
-      out.error(`failed to spawn ${desc}: ${err.message}`);
-      reject(err);
-    }
-  });
-}
-
-async function dotnet(args) {
-  return spawnAsync('dotnet', args, {}, console);
-}
-
-function sign(filePath) {
-  if (process.env['SIGN_TOOL'] !== undefined) {
-    return spawnAsync(process.env['SIGN_TOOL'], ['sign', '/sha1', process.env['SIGN_THUMBPRINT'], '/td', 'sha256', '/fd', 'sha256', '/tr', 'http://timestamp.comodoca.com', filePath]);
-  }
+function userHasDotNet() {
+  return  dotnet?.runtimeVersion !== null && semver.satisfies(dotnet?.runtimeVersion, '>=6.0.0');
 }
 
 async function main() {
-  try {
-    const buildType = debugBuild ? 'Debug' : 'Release';
-    await fsExtra.remove(path.join(__dirname, 'dist'));
-    const args = ['publish', 'ModInstallerIPC', '-c', buildType, '--no-self-contained', '-r', 'win-x64', '-o', 'dist'];
-    if (!debugBuild) {
-      args.push('/p:DebugType=None', '/p:DebugSymbols=false');
-    }
-    try {
-      await dotnet(args);
-    } catch (err) {
-      // the build may fail because of locked files (sigh) so just try again...
-      await new Promise((resolve) => setTimeout(resolve, 500));
-      await dotnet(args);
-    }
-    await sign('dist\\ModInstallerIPC.exe');
-  } catch (err) {
-    const error = ERROR_CODE_HANDLER[err?.code] !== undefined
-      ? ERROR_CODE_HANDLER[err.code].genError() : err;
-    console.error('build failed:', error.message);
-    const exitCode = !err.code ? -1 : err.code;
-    process.exit(exitCode);
-  }
+  return true;
 }
 
 main();
--- index.js
+++ index.js
@@ -1,117 +1,31 @@
-const cp = require('child_process');
+//const cp = require('child_process');
 const path = require('path');
 const fs = require('fs-extra');
-const os = require('os');
+const dotnet = require('dotnet');
 
-let winapi;
-try {
-  winapi = require('winapi-bindings');
-} catch (err) {}
-
-async function startRegular(exePath, args, onExit, onStdout) {
-  return new Promise((resolve, reject) => {
-    const proc = cp.spawn(exePath, args)
-      .on('error', err => {
-        reject?.(err);
-        resolve = reject = undefined;
-      })
-      .on('exit', (code, signal) => {
-        if (code === 0x80131700) {
-          reject?.(new Error('No compatible .Net Framework, you need .Net framework 4.6 or newer'));
-        } else if (code !== null) {
-          reject?.(new Error(`Failed to run fomod installer. Errorcode ${code.toString(16)}`));
-        } else {
-          reject?.(new Error(`The fomod installer was terminated. Signal: ${signal}`));
-        }
-        resolve = reject = undefined;
-        onExit(code);
-      });
-
-    proc.stdout.on('data', dat => onStdout(dat.toString()));
-    proc.stderr.on('data', dat => onStdout(dat.toString()));
-
-    // resolve slightly delayed to allow the error event to be triggered if the process fails to
-    // start. Unfortunately cp.spawn seems to flip a coin on whether it reports events at all or not.
-    setTimeout(() => {
-      if ((proc.exitCode !== null) && (proc.exitCode !== 0)) {
-        reject?.(new Error('Failed to spawn fomod installer'));
-      } else {
-        resolve?.(proc.pid);
-      }
-      resolve = reject = undefined;
-    }, 100);
-  });
-}
-
-async function startSandboxed(containerName, id, exePath, cwd, args, onExit, onStdout) {
-  return new Promise((resolve, reject) => {
-    // in case the container wasn't cleaned up before
-    try {
-      winapi.DeleteAppContainer(containerName);
-      winapi.CreateAppContainer(containerName, 'FOMOD', 'Container for fomod installers');
-      process.on('exit', () => {
-        winapi.DeleteAppContainer(containerName);
-      });
-
-      winapi.GrantAppContainer(containerName, `\\\\?\\pipe\\${id}`, 'named_pipe', ['all_access']);
-      winapi.GrantAppContainer(containerName, `\\\\?\\pipe\\${id}_reply`, 'named_pipe', ['all_access']);
-
-      try {
-        const pid = winapi.RunInContainer(containerName, `${exePath} ${args.join(' ')}`, cwd, onExit, onStdout);
-        resolve(pid);
-      } catch (err) {
-        // I think this may be caused by an AV scanning the files after we copied them
-        setTimeout(() => {
-          try {
-            const pid = winapi.RunInContainer(containerName, `${exePath} ${args.join(' ')}`, cwd, onExit, onStdout);
-            resolve(pid);
-          } catch (err) {
-            reject(err);
-          }
-        }, 1000);
-      }
-    } catch (err) {
-      reject(err);
+const initAssemblyResolver = async () => {
+  dotnet.addListener('resolving', (name, version, resolve) => {
+    // Lets try to keep this event listener as light, static and synchronous
+    //  as possible for the sake of our future selves.
+    const recursiveRead = (directory) => {
+      const files = fs.readdirSync(directory)
+        .map(file => path.join(directory, file))
+        .filter(file => fs.statSync(file).isFile() && path.extname(file) === '.dll');
+    
+      return files.concat(
+        ...files.map(file => recursiveRead(file))
+          .flatMap(files => files)
+      );
+    }
+    const assemblies = recursiveRead(__dirname);
+    const potentialMatch = assemblies.find(file => path.basename(file).toLowerCase() === name.toLowerCase());
+    if (potentialMatch) {
+      resolve(potentialMatch)
     }
   });
-}
-
-async function startLowIntegrity(exePath, cwd, args, onExit, onStdout) {
-  return new Promise((resolve, reject) => {
-    try {
-      const pid = winapi.CreateProcessWithIntegrity(`${exePath} ${args.join(' ')}`, cwd, 'low', onExit, onStdout);
-      resolve(pid);
-    } catch (err) {
-      reject(err);
-    }
-  });
-}
-
-async function createIPC(usePipe, id, onExit, onStdout, containerName, lowIntegrity) {
-  // it does actually get named .exe on linux as well
-  const exeName = 'ModInstallerIPC.exe';
-
-  let cwd = path.join(__dirname.replace('app.asar', 'app.asar.unpacked'), 'dist');
-  let exePath = path.join(cwd, exeName);
-
-  const args = [id];
-  if (usePipe) {
-    args.push('--pipe');
-  }
-
-  if (winapi !== undefined) {
-    // on windows
-    if ((winapi?.SupportsAppContainer?.() === true) && (containerName !== undefined)) {
-      return startSandboxed(containerName, id, exePath, cwd, args, onExit, onStdout);
-    } else if (lowIntegrity) {
-      return startLowIntegrity(exePath, cwd, args, onExit, onStdout);
-    }
-  }
-  // fallback for other OSes and if the above solutions are disabled
-  return startRegular(exePath, args, onExit, onStdout);
-}
+};
 
 module.exports = {
   __esModule: true,
-  createIPC,
+  initAssemblyResolver,
 };
