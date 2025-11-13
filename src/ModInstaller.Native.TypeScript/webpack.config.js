cc5c498ec42c91ec06bbc08ed2706c57c2e8afa8
Vitalii Mikhailov <personal@aragas.org>
2025-11-13 15:45:54 +0200

    fix

--- src/ModInstaller.Native.TypeScript/webpack.config.js
+++ src/ModInstaller.Native.TypeScript/webpack.config.js
@@ -2,7 +2,7 @@ const webpack = require('vortex-api/bin/webpack').default;
 
 const config = webpack('fomod-installer-native', __dirname, 5);
 
-config.externals['./../../build/modinstaller.node'] = './modinstaller.node';
+config.externals['./../build/modinstaller.node'] = './modinstaller.node';
 
 module.exports = config;
 
